69f1c267dcfdb538ad5e4286dccdc0b81735cbd3
teacherIan <ian_malloy@ssis.asia>
2022-08-22 13:50:33 +0800

    css stuff

--- experience/htmlController.js
+++ experience/htmlController.js
@@ -41,22 +41,16 @@ arrayOfItems.forEach((item, i) => {
         item.style.height = '150px';
       });
     }
-
-    // if (i == 0) {
-    //   projects.style.height = '60%';
-    //   demoLessons.style.height = '20%';
-    //   resume.style.height = '20%';
-    // }
-    // if (i == 1) {
-    //   projects.style.height = '20%';
-    //   demoLessons.style.height = '60%';
-    //   resume.style.height = '20%';
-    // }
-    // if (i == 2) {
-    //   projects.style.height = '20%';
-    //   demoLessons.style.height = '20%';
-    //   resume.style.height = '60%';
-    // }
+    if (i == 1) {
+      projectThumbnails.forEach((item) => {
+        item.style.height = '0px';
+      });
+    }
+    if (i == 2) {
+      projectThumbnails.forEach((item) => {
+        item.style.height = '0px';
+      });
+    }
   });
 });
 
@@ -67,22 +61,30 @@ const removeSelected = (selectedElement) => {
   });
 };
 
-console.log(arrayOfItemDiv);
-
 arrayOfItemDiv.forEach((item, i) => {
   item.addEventListener('mouseover', () => {
-    if (selected == 0 && i < 3) {
+    arrayOfItemDiv.forEach((innerItem) => {
+      innerItem.style.opacity = '0.5';
+    });
+
+    if (selected == 0 && i < 4) {
       projects.style.height = '150vh';
       textArray[0].innerHTML = 'this is the description for ' + i;
       if (i == 0) {
         textArray[0].innerHTML = ` <div class='inner-header'> House Point Counter </div> I wanted to create more interest in our school 'house' system, especially for younger students, while giving older students an example of how they can use code to improve their school. This counter does both these things, and has been well received by students and facility.  Can easily be adapted for different schools and situations.  This project goes along with its backend project, to give teachers an easy way to add and remove points while keeping tabs on why points were add or removed.  click  ${'<a target="_blank" href="https://houses-nine.vercel.app/" class="link">here</a>'} for a demo page or ${'<a target="_blank" href="https://github.com/teacherIan/front-end-update" class="link">here</a>'} for the git repository.`;
-        // item.style.border = `5px solid black`;
+        arrayOfItemDiv[0].style.opacity = '1';
       }
       if (i == 1) {
-        textArray[0].innerHTML = `<div class='inner-header'> Computer Science Learning Platform </div> I wanted to create a learning platform for students that would leave the learner the skills needed to create programs the way professionals do.  Although under construction, my vision is for a students to be able to fully do a six month course using only this site, and by the end, have the abilities to use an IDE.  By able to create projects from scratch. Use basic git commands and deploy their programs. Although unfinished, you can view the site here ${'<a target="_blank" href="https://houses-nine.vercel.app/" class="link">here</a>'} for a demo page or ${'<a target="_blank" href="https://github.com/teacherIan/front-end-update" class="link">here</a>'} for the git repository.`;
+        textArray[0].innerHTML = `<div class='inner-header'> Computer Science Learning Platform </div> I wanted to create a learning platform for students that would leave the learner the skills needed to create programs the way professionals do.  Although under construction, my vision is for a students to be able to fully do a six month course using only this site, and by the end, have the abilities to use an IDE.  By able to create projects from scratch. Use basic git commands and deploy their programs. Although unfinished, you can view the site here ${'<a target="_blank" href="https://school-learn-to-program.vercel.app/" class="link">here</a>'} for a demo page or ${'<a target="_blank" href="https://github.com/teacherIan/front-end-update" class="link">here</a>'} for the git repository.`;
+        arrayOfItemDiv[1].style.opacity = '1';
       }
       if (i == 2) {
-        textArray[0].innerHTML = `<div class='inner-header'> 3D Math/CS challenge </div>This project is designed to create interest in computer science while also being a fun and challenging house competition.  Students are asked to create a design mathematical design using using a template I created which abstracted away some of the difficulty of coding .  Students with no coding experience can also take part in the challenge. Here is the repo for the starter project ${'<a target="_blank" href="https://github.com/teacherIan/steam-house-competition-starter" class="link">here</a>'} of view any of these example demos: ${'<a target="_blank" href="https://github.com/teacherIan/front-end-update" class="link">example A</a>'}, ${'<a target="_blank" href="https://github.com/teacherIan/front-end-update" class="link">example B</a>'}, or   ${'<a target="_blank" href="https://github.com/teacherIan/front-end-update" class="link">example C</a>'} `;
+        textArray[0].innerHTML = `<div class='inner-header'> 3D Math/CS challenge </div>This project is designed to create interest in computer science while also being a fun and challenging house competition.  Students are asked to create a design mathematical design using using a template I created which abstracted away some of the difficulty of coding .  Students with no coding experience can also take part in the challenge. Here is the repo for the starter project ${'<a target="_blank" href="https://github.com/teacherIan/steam-house-competition-starter" class="link">here</a>'} of view any of these example demos: ${'<a target="_blank" href="https://steam-house-competition-starter-demo-a.vercel.app/" class="link">example A</a>'}, ${'<a target="_blank" href="https://steam-house-competition-starter-demo-b.vercel.app/" class="link">example B</a>'}, or   ${'<a target="_blank" href="https://steam-house-competition-starter-demo-c.vercel.app/" class="link">example C</a>'} `;
+        arrayOfItemDiv[2].style.opacity = '1';
+      }
+      if (i == 3) {
+        textArray[0].innerHTML = `<div class='inner-header'> IB/IG Folder System </div>This project is designed to help teachers create the correct folder structure for sending example work into IB/IG.  Can be used with any sized class. Click  ${'<a target="_blank" href="https://ib-ig-file-system.herokuapp.com/" class="link">here</a>'} to view a demo site, or  ${'<a target="_blank" href="https://github.com/teacherIan/IB_IG_class_names" class="link">here</a>'}, to view the repo`;
+        arrayOfItemDiv[3].style.opacity = '1';
       }
     } else if (selected == 1 && i < 6) {
       console.log('Current item hovered = ' + i);
